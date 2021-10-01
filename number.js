b89655b0eba3730202e4c59b7058b77caf399071
eugene177 <vbxer2019@gmail.com>
2021-10-01 22:01:06 +0300

    second commit

---
+++ number.js
@@ -0,0 +1,22 @@
+// // Задача №1.
+// const miniNum = 1.005 + Number.EPSILON
+// let numOne = Math.round(miniNum * 100) / 100
+// console.log(numOne)
+
+
+// // Задача №2.
+// let value = "135.58px"
+// console.log(parseFloat(value))
+
+
+
+// // Задача №3.
+// let value = 58 + "Фрилансер"
+// if ((isNaN(value))) {
+//   console.log('Результат выражения NaN')
+// }
+
+
+
+// Задача №5.
+console.log(Math.floor(58.858))
---
+++ string.js
@@ -0,0 +1,22 @@
+// // Задача №1.
+// let fls = 'Фрилансер'
+// let text = `Привет! Я ${fls}`
+// console.log(text)
+
+
+
+// // Задача №2.
+// let text = 'Фрилансер'
+// console.log(text[5])
+
+
+
+// // Задача №3.
+// let text = 123 + +'456'
+// let text1 = 123 + (Number("456"))
+// console.log(text)
+// console.log(text1)
+
+
+
+// Задача №4.
