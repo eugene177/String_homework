1df7000d68465697c66f236760c990cb320cbd90
eugene177 <vbxer2019@gmail.com>
2021-10-25 10:47:21 +0300

    Last commit

--- number.js
+++ number.js
@@ -1,23 +1,21 @@
-// // Задача №1.
-// const miniNum = 1.005 + Number.EPSILON
-// let numOne = Math.round(miniNum * 100) / 100
+// // Задача №1
+// let numOne = Math.ceil(1.005 * 100) / 100
 // console.log(numOne)
 
-
-
-// // Задача №2.
-// let value = "135.58px"
+// // Задача №2
+// let value = '135.58px'
 // console.log(parseFloat(value))
 
-
-
-// // Задача №3.
-// let value = 58 + "Фрилансер"
-// if ((isNaN(value))) {
-//   console.log('Результат выражения NaN')
+// // Задача №3
+// let value = 58 + 'Фрилансер'
+// if (isNaN(value)) {
+//   console.log('Result is NaN!')
+// } else {
+//   console.log('Result is not NaN')
 // }
 
+// // Задача №4
+// console.log(Math.max(10, 58, 39, -150, 0))
 
-
-// Задача №5.
-console.log(Math.floor(58.858))
+// // Задача №5
+// console.log(Math.floor(58.858))
--- string.js
+++ string.js
@@ -1,39 +1,26 @@
-// // Задача №1.
-// let fls = 'Фрилансер'
-// let text = `Привет! Я ${fls}`
+// // Задача №1
+// let fls = 'фрилансер'
+// let text = `Привет! Я ${fls}!`
 // console.log(text)
 
-
-
-// // Задача №2.
+// // Задача №2
 // let text = 'Фрилансер'
 // console.log(text[5])
 
-
-
-// // Задача №3.
+// // Задача №3
 // let text = 123 + +'456'
-// let text1 = 123 + (Number("456"))
-// console.log(text)
-// console.log(text1)
+// let text1 = 123 + Number('456')
+// console.log(`${text}, first way`)
+// console.log(`${text1}, second way`)
 
-
-
-// // Задача №4.
-// let text = 'Фрилансер'
+// // Задача №4
+// let text = 'фрилансер'
 // console.log(text.toUpperCase())
 
-
-
-// // Задача №5.
+// // Задача №5
 // let text = 'Фрилансер'
 // console.log(text.slice(3, 6))
 
-
-
-// Задача №6.
-let text = 'Фрилансер'
-console.log(text.includes('лан', 3))
-
-
-
+// // Задача №6
+// let text = 'Фрилансер'
+// console.log(text.includes('лан', 3))
